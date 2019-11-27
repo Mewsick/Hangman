074cd47583f1bfb8a3afc531f6505b24fc138b2f
Mewsick <56956191+Mewsick@users.noreply.github.com>
2019-11-27 19:25:58 +0100

    Add files via upload

---
+++ Hangman/Hangman/app/src/main/java/com/example/hangman/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hangman.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
